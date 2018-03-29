596b6653b3130b87362fc65c7756342233c382a6
Philip Chase <pbc@ufl.edu>
2018-03-29 14:11:35 -0400

    Improve description of valued projects

--- describe_projects/create_a_view_of_valued_projects.sql
+++ describe_projects/create_a_view_of_valued_projects.sql
@@ -1,4 +1,12 @@
 -- Create a view of valued projects
+-- Theese are projects that are:
+--   in dev or prod status
+--   project is not "Practice just for fun"
+--   titles do not look like copies of projects
+--   > 1 yr old
+--   > 1000 saved attributes
+--   < 180 days since last logged event
+
 create or replace view uf_valued_projects as
 select rcp.project_id
 from redcap_projects as rcp
