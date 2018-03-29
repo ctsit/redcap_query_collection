8974878cf48cba8ef3cc5ab9c6421c502680be2a
Philip Chase <pbc@ufl.edu>
2018-03-29 10:20:21 -0400

    Use uf_project_ownership view in queries in generate_project_billing_record.sql

--- describe_projects/generate_project_billing_record.sql
+++ describe_projects/generate_project_billing_record.sql
@@ -16,14 +16,13 @@ order by rcp.project_id desc;
 
 
 -- active users and owners on projects above
-select rcp.project_id, rcp.app_title, rcp.project_name, rcp.creation_time, rcui_owner.user_email as owner_email, rcui.user_email
+select rcp.project_id, rcp.app_title, rcp.project_name, rcp.creation_time, ufpo.email as owner_email, rcui.user_email
 from redcap_projects as rcp
 inner join redcap_project_stats as rcps on (rcp.project_id = rcps.project_id)
 left join redcap_user_rights as rcur on (rcp.project_id = rcur.project_id)
 left join redcap_user_roles as rcuro on (rcp.project_id = rcuro.project_id and rcur.role_id = rcuro.role_id)
 inner join redcap_user_information as rcui on (rcur.username = rcui.username)
-left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
-inner join redcap_user_information as rcui_owner on (rcpo.username = rcui_owner.username)
+left join uf_project_ownership as ufpo on (rcp.project_id = ufpo.project_id)
 where 
 	rcp.status in (0,1)
     and rcp.purpose != 0
@@ -36,12 +35,11 @@ where
     and datediff(now(), rcui.user_lastlogin) < 180
 order by rcp.project_id desc;
 
--- projects in dev or prod status, > 1 yr old, > 500 attributes, <180 days since last loged event and good titles
-select rcui_owner.user_email as owner_email, count(*)
+-- Count of significant projects by owner email
+select ufpo.email as owner_email, count(*)
 from redcap_projects as rcp
 inner join redcap_project_stats as rcps on (rcp.project_id = rcps.project_id)
-left join rcpo_test as rcpo on (rcp.project_id = rcpo.pid)
-left join redcap_user_information as rcui_owner on (rcpo.username = rcui_owner.username)
+left join uf_project_ownership as ufpo on (rcp.project_id = ufpo.project_id)
 where 
 	rcp.status in (0,1)
     and rcp.purpose != 0
@@ -50,4 +48,4 @@ where
     and datediff(now(), rcp.creation_time) > 365
     and rcps.saved_attribute_count > 1000
     and datediff(now(), rcp.last_logged_event) < 180
-group by owner_email;
+group by email;
