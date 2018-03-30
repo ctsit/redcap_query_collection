ffdcc77c0d95d1f1a980d51667707bf3bcbd73d8
Philip Chase <pbc@ufl.edu>
2018-03-30 17:03:25 -0400

    Add fiscal fields to project billing report

--- describe_projects/contact_and_size_data_for_uf_valued_projects.sql
+++ describe_projects/contact_and_size_data_for_uf_valued_projects.sql
@@ -6,7 +6,8 @@ select rcp.project_id, rcp.app_title, rcp.creation_time,
        ufvp.uploaded_file_count, ufvp.total_file_storage_in_mb,
        ufpo.firstname as owner_firstname, ufpo.lastname as owner_lastname, ufpo.email as owner_email,
        project_pi_firstname, project_pi_lastname, project_irb_number,
-       ufpo.firstname as project_user_firstname, ufpo.lastname as project_user_lastname, ufpo.email as project_user_email_address
+       ufpo.firstname as project_user_firstname, ufpo.lastname as project_user_lastname, ufpo.email as project_user_email_address,
+       date_format(date_add(now(), interval 1 year), "%Y-%m-%d") as period_ending_date, '' as payment_date, '' as payment_transaction_id
 from redcap_projects as rcp
 inner join uf_valued_projects as ufvp on (rcp.project_id = ufvp.project_id)
 left join uf_project_ownership as ufpo on (rcp.project_id = ufpo.project_id)
@@ -17,7 +18,8 @@ select rcp.project_id, rcp.app_title, rcp.creation_time,
        ufvp.uploaded_file_count, ufvp.total_file_storage_in_mb,
        ufpo.firstname as owner_firstname, ufpo.lastname as owner_lastname, ufpo.email as owner_email,
        project_pi_firstname, project_pi_lastname, project_irb_number,
-       rcui.user_firstname as project_user_firstname, rcui.user_lastname as project_user_lastname, rcui.user_email as project_user_email_address
+       rcui.user_firstname as project_user_firstname, rcui.user_lastname as project_user_lastname, rcui.user_email as project_user_email_address,
+       date_format(date_add(now(), interval 1 year), "%Y-%m-%d") as period_ending_date, '' as payment_date, '' as payment_transaction_id
 from redcap_projects as rcp
 inner join uf_valued_projects as ufvp on (rcp.project_id = ufvp.project_id)
 left join redcap_user_rights as rcur on (rcp.project_id = rcur.project_id)
