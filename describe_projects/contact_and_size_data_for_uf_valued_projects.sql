5d73666c12577e4ce1ca91d5bd2110db6c334dae
Philip Chase <pbc@ufl.edu>
2018-03-30 16:47:59 -0400

    Add measures of project size to the project billing report now named contact_and_size_data_for_uf_valued_projects.sql

--- describe_projects/contact_and_size_data_for_uf_valued_projects.sql
+++ describe_projects/contact_and_size_data_for_uf_valued_projects.sql
@@ -1,7 +1,9 @@
 -- Generate the project billing record
 
--- email_contacts_for_uf_valued_projects.csv
+-- contact_and_size_data_for_uf_valued_projects.csv
 select rcp.project_id, rcp.app_title, rcp.creation_time,
+       ufvp.saved_attribute_count, ufvp.record_count,
+       ufvp.uploaded_file_count, ufvp.total_file_storage_in_mb,
        ufpo.firstname as owner_firstname, ufpo.lastname as owner_lastname, ufpo.email as owner_email,
        project_pi_firstname, project_pi_lastname, project_irb_number,
        ufpo.firstname as project_user_firstname, ufpo.lastname as project_user_lastname, ufpo.email as project_user_email_address
@@ -11,6 +13,8 @@ left join uf_project_ownership as ufpo on (rcp.project_id = ufpo.project_id)
 where ufpo.email is not NULL
 UNION
 select rcp.project_id, rcp.app_title, rcp.creation_time,
+       ufvp.saved_attribute_count, ufvp.record_count,
+       ufvp.uploaded_file_count, ufvp.total_file_storage_in_mb,
        ufpo.firstname as owner_firstname, ufpo.lastname as owner_lastname, ufpo.email as owner_email,
        project_pi_firstname, project_pi_lastname, project_irb_number,
        rcui.user_firstname as project_user_firstname, rcui.user_lastname as project_user_lastname, rcui.user_email as project_user_email_address
