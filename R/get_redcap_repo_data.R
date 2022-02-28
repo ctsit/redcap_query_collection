1e5103478d68168a62c936c3b1f90503b0775673
Michael Bentz <mbentz@ufl.edu>
2022-02-28 11:06:22 -0500

    Fix scrape_redcap_repo_data

--- R/get_redcap_repo_data.R
+++ R/get_redcap_repo_data.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(rvest)
 
-scrape_redcap_repo_data <- function(create_csv = FALSE){
+scrape_redcap_repo_data <- function(create_csv = FALSE) {
 
-  url <-  read_html("https://redcap.vanderbilt.edu/consortium/modules/index.php")
+  url <- read_html("https://redcap.vanderbilt.edu/consortium/modules/index.php")
 
   # using html_table results in loss of github links
   # and require extraction of all fields other than dates and downloads
@@ -22,61 +22,61 @@ scrape_redcap_repo_data <- function(create_csv = FALSE){
   }
 
   private_git_repos <- list()
-  for (entry in c(1:number_of_entries)){
+  for (entry in c(1:number_of_entries)) {
     private_git_repos[[entry]] <- get_private_git_repos()
   }
 
   private_git_repos <- bind_rows(!!!private_git_repos) %>%
     filter(private_git_repo)
 
-  scrape_redcap_repo <- function(entry){
+  scrape_redcap_repo <- function(entry) {
     title <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(1) > div:nth-child(1) > span') %>%
-      html_text()
+    html_nodes('td:nth-child(1) > div:nth-child(1) > span') %>%
+    html_text()
 
     deployed <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(1) > div:nth-child(1) > i') %>%
-      html_text()
+    html_nodes('td:nth-child(1) > div:nth-child(1) > i') %>%
+    html_text()
 
     github_url <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(1) > div:nth-child(1) > a') %>%
-      html_attr("href")
+    html_nodes('td:nth-child(1) > div:nth-child(2) > a:nth-child(1)') %>%
+    html_attr("href")
 
     description <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(1) > div:nth-child(2)') %>%
-      html_text()
+    html_nodes('td:nth-child(1) > div:nth-child(3)') %>%
+    html_text()
 
     date_added <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(2)') %>%
-      html_text()
+    html_nodes('td:nth-child(2)') %>%
+    html_text()
 
     last_updated <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(3)') %>%
-      html_text()
+    html_nodes('td:nth-child(3)') %>%
+    html_text()
 
     downloads <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(4)') %>%
-      html_text() %>%
-      str_remove(., ",") %>%
-      as.integer()
+    html_nodes('td:nth-child(4)') %>%
+    html_text() %>%
+    str_remove(., ",") %>%
+    as.integer()
 
     author <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(1) > div:nth-child(3) > a') %>%
-      html_text() %>%
-      unique() %>%
-      paste(collapse = ", ")
+    html_nodes('td:nth-child(1) > div:nth-child(4) > a') %>%
+    html_text() %>%
+    unique() %>%
+    paste(collapse = ", ")
 
     author_email <- redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(1) > div:nth-child(3) > a') %>%
-      html_attr("href") %>%
-      unique() %>%
-      paste(collapse = ", ")
+    html_nodes('td:nth-child(1) > div:nth-child(4) > a') %>%
+    html_attr("href") %>%
+    unique() %>%
+    paste(collapse = ", ")
 
-    institution <-  redcap_modules[[entry]] %>%
-      html_nodes('td:nth-child(1) > div:nth-child(3) > span') %>%
-      html_text() %>%
-      unique() %>%
-      paste(collapse = ", ")
+    institution <- redcap_modules[[entry]] %>%
+    html_nodes('td:nth-child(1) > div:nth-child(4) > span') %>%
+    html_text() %>%
+    unique() %>%
+    paste(collapse = ", ")
 
     tibble(title = title, deployed = deployed, github_url = github_url,
            description = description, date_added = date_added, last_updated = last_updated,
@@ -91,7 +91,7 @@ scrape_redcap_repo_data <- function(create_csv = FALSE){
   }
 
   redcap_repo_data <- list()
-  for (entry in redcap_repo_entries){
+  for (entry in redcap_repo_entries) {
     redcap_repo_data[[entry]] <- scrape_redcap_repo(entry)
   }
 
@@ -102,7 +102,7 @@ scrape_redcap_repo_data <- function(create_csv = FALSE){
            deployed = str_remove(deployed, "_v\\d.+")) %>%
     select(title, deployed, version, everything())
 
-  if (create_csv){
+  if (create_csv) {
     write.csv(redcap_repo_data, "redcap_repo_data.csv", row.names = F, na = "")
   }
 
